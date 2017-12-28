31b4379f11885b8a6c4da4bd5cc9a06be9416bf3
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-28 12:40:47 -0600

    adds initializon to analyze

--- faceAPI.js
+++ faceAPI.js
@@ -1,1 +1,26 @@
-const APIKey = "";
+const APIKey = "30341e7fca68433ca4b17627450ca5f9";
+const faceIDLink = "https://westusff.api.cognitive.microsoft.com/face/v1.0/detect"
+
+document.getElementById("analyze").addEventListener("click", analyze);
+
+function analyze(){
+  const reqBody = {
+    "url" : document.getElementById("imageURL").value
+  }
+
+  const header = new Headers({
+    "Content-Type": "application/json",
+    "Ocp-Apim-Subscription-Key" : APIKey
+  })
+
+  const initObj = {
+    method: 'POST',
+    headers: header,
+    mode: "cors",
+    body: reqBody
+    // body: JSON.stringify(reqBody)
+  }
+
+  const request = new Request(faceIDLink, initObj)
+
+}
