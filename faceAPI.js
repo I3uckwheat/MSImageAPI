56589542cbf39ff624d9b1c17574ed57f3e34ce3
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-28 13:01:38 -0600

    appends api response accordingly

--- faceAPI.js
+++ faceAPI.js
@@ -1,5 +1,5 @@
 const APIKey = "30341e7fca68433ca4b17627450ca5f9";
-const faceIDLink = "https://westusff.api.cognitive.microsoft.com/face/v1.0/detect"
+const faceIDLink = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceAttributes=age,gender"
 
 document.getElementById("analyze").addEventListener("click", analyze);
 
@@ -17,10 +17,28 @@ function analyze(){
     method: 'POST',
     headers: header,
     mode: "cors",
-    body: reqBody
-    // body: JSON.stringify(reqBody)
+    body: JSON.stringify(reqBody)
   }
 
   const request = new Request(faceIDLink, initObj)
+
+  fetch(request)
+    .then(response => {return response.json()})
+    .then(appendAttributes)
+    .catch(error => console.log(error));
+}
+
+function appendAttributes(attributes) {
+  const attributesDiv = document.getElementById("attributes");
+  attributesDiv.innerHTML = "";
+
+  const age = document.createElement('p')
+  age.textContent = attributes[0].faceAttributes.age
+
+  const gender = document.createElement('p')
+  gender.textContent = attributes[0].faceAttributes.gender
+
+  attributesDiv.appendChild(age);
+  attributesDiv.appendChild(gender);
 
 }
